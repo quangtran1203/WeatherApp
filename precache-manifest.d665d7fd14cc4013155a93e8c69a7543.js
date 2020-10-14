bc126ea23dfe806467552a0dcf8e7c20dc76d234
quangtran1203 <harrytran1232001@gmail.com>
2020-10-14 00:28:10 -0400

    Updates

---
+++ precache-manifest.d665d7fd14cc4013155a93e8c69a7543.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8a88308c5e024cbc6dec3154f680cef0",
+    "url": "/WeatherApp/index.html"
+  },
+  {
+    "revision": "af811dc8737fd98f4179",
+    "url": "/WeatherApp/static/css/main.b1f72978.chunk.css"
+  },
+  {
+    "revision": "d36834ddd497d19b90df",
+    "url": "/WeatherApp/static/js/2.2e93365d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/WeatherApp/static/js/2.2e93365d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "af811dc8737fd98f4179",
+    "url": "/WeatherApp/static/js/main.8dd11ca6.chunk.js"
+  },
+  {
+    "revision": "f34a18f0592fbbc8ce7e",
+    "url": "/WeatherApp/static/js/runtime-main.dcd8b410.js"
+  },
+  {
+    "revision": "9836b0bb9a2dc65c77a2eaee55668ca6",
+    "url": "/WeatherApp/static/media/cloud.9836b0bb.jpg"
+  },
+  {
+    "revision": "63bf4c470fadd4772272abd52dc496aa",
+    "url": "/WeatherApp/static/media/foggy.63bf4c47.jpg"
+  },
+  {
+    "revision": "d5b633045d67b8c9329dc80eb91e5d6e",
+    "url": "/WeatherApp/static/media/horizon.d5b63304.jpg"
+  },
+  {
+    "revision": "59e5a929f4537a5a7fe24e5ad5d97670",
+    "url": "/WeatherApp/static/media/pink.59e5a929.jpg"
+  },
+  {
+    "revision": "f8cb08cf968bc8343b6469d77ab4ff8d",
+    "url": "/WeatherApp/static/media/warm.f8cb08cf.jpg"
+  }
+]);
